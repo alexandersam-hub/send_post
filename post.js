05510ca10e1339ec4385d386898242ce994f1dcf
stan1i <alexandersamedov@gmail.com>
2022-05-24 19:08:51 +0300

    first commit20

--- post.js
+++ post.js
@@ -14,13 +14,9 @@ const app = express()
 
 app.use(express.json({ limit: "50mb" }))
 app.use('/api/post', postRouter)
-
+app.use(require('morgan')('dev'))
 app.use(
-    cors({
-        origin: "*",
-        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-        preflightContinue: false
-    })
+    cors()
 );
 // app.all('*', function(req, res, next) {
 //     res.header('Access-Control-Allow-Origin', '*');
